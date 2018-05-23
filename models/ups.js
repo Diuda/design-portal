e4d9c0385592e734dc6300d81b694620d2316397
Diuda <divyanshu.1996@gmail.com>
2018-05-23 08:29:26 +0530

    ups data model created

---
+++ models/ups.js
@@ -0,0 +1,39 @@
+var mongoose = require('mongoose');
+
+var mongoosePaginate = require('mongoose-paginate');
+
+var Schema = mongoose.Schema;
+
+
+var upsSchema = new Schema({
+
+    input: {    
+
+        power: { type: Number, required: true },
+        batteryRuntime: { type: Number, required: true },
+        upsType: { type: String, required: true }, 
+        region: { type: String, required: true },
+        country: { type: String, required: true },
+        bypass: { type: Boolean, required: true },
+        redundancyUnit: { type: Number, required: true, min:0, max:10 },
+        pf: { type: Number, required: true, min:0, max:1 }
+        
+    },
+
+    output: {
+
+        nameUPS: { type: String, required: true },
+        voltage: { type: Number, required: true },
+        upsCount: { type: Number, required: true }
+
+    }
+
+})
+
+
+
+// upsSchema.plugin(mongoosePaginate);
+
+const ups = mongoose.model('ups', upsSchema)
+
+module.exports = ups;
