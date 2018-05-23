7dde4a0189324882aded3af4b00f792de6b1eae5
Diuda <divyanshu.1996@gmail.com>
2018-05-23 16:47:20 +0530

    post request to search api end pooint

--- angular-stuff/src/app/services/fetch-data.service.ts
+++ angular-stuff/src/app/services/fetch-data.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { map } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class FetchDataService {
 
-  constructor() { }
+  UPSparams: any;
+
+  constructor(private https: Http) { }
+
+  getUPS(UPSparams) {
+
+    var headers = new Headers();
+    headers.append('Content-Type', 'application/json')
+    console.log(UPSparams);
+
+    return this.https.post('http://localhost:3000/search', UPSparams, { headers: headers }).pipe(map(res=>res.json()));
+
+  }
+
+
 }
