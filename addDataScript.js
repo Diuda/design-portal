3633fb16b0de5bb15a2351bdb7fa2d2a1f01457c
Diuda <divyanshu.1996@gmail.com>
2018-06-01 00:02:17 +0530

    adding input fields to mongo

--- addDataScript.js
+++ addDataScript.js
@@ -5,8 +5,10 @@ var csv = require('csvtojson')
 
 var mongoogse = require('mongoose');
 
+var UPS = require('./models/ups.js');
 
-var p, btR, upsType, region, c, b, rU, pwf, v;
+
+var p, btR, upsType, region, c, b, rU, pwf, v, rz;
 exports.addDataToMongo = () => {
 
     console.log("hello")
@@ -16,6 +18,8 @@ exports.addDataToMongo = () => {
         console.log(jsonObj.length)
         res.send("done")
         var temp1 = jsonObj[0].RzId
+        var tCountry = jsonObj[0].CtryId
+        var tRegion = jsonObj[0].RgDesc
         for(i in jsonObj){
             // console.log(data[0])
             if(temp1 == jsonObj[i].RzId){
@@ -23,29 +27,61 @@ exports.addDataToMongo = () => {
                     upsType = jsonObj[i].RzevValue.split(" ", 2).join(" ")
                     var t = jsonObj[i].RzevValue.split(" ", 3)
                     p = t[2]
+                    continue
                 }
                 else if(jsonObj[i].EvId == 33){
                     v = jsonObj[i].RzevValue
+                    continue
                 }
                 else if(jsonObj[i].EvId == 100){
                     pwf = jsonObj[i].RzevValue
+                    continue
                 }
                 else if(jsonObj[i].EvId == 101){
                     btR = jsonObj[i].RzevValue
+                    continue
                 }
                 else if(jsonObj[i].EvId == 102){
                     b = jsonObj[i].RzevValue
+                    continue
                 }
                 else if(jsonObj[i].EvId == 103){
-                    btR = jsonObj[i].RzevValue
+                    rU = jsonObj[i].RzevValue
+                    continue
+                }            
+
+            }
+            else {
+                rz = temp1;
+                temp1 = jsonObj[i].RzId;
+                c = tCountry;
+                tCountry = jsonObj[i].CtryId
+                region = tRegion;
+                tRegion = jsonObj[i].RgDesc
+
+                var addUPS = new UPS({
+                    roomZoneId: rz,
+                    input: {
+                        power: p,
+                        voltage: v,
+                        batteryRuntime: btR,
+                        upsType: upsType,
+                        region: region,
+                        country: c,
+                        bypass: b,
+                        redundancyUnit: rU,
+                        pf: pwf
+                    }
+                })
+
+                if(jsonObj[i].EvId == 22){
+                    upsType = jsonObj[i].RzevValue.split(" ", 2).join(" ")
+                    var t = jsonObj[i].RzevValue.split(" ", 3)
+                    p = t[2]
+                    continue
                 }
 
             }
-            var addUPS = new addUPS({
-                input: {
-                    country: data.
-                }
-            })
         }
     })
 
