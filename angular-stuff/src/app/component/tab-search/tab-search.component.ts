9ba4b17b39fdb1456e15cd2183e8004ee07bf59a
Diuda <divyanshu.1996@gmail.com>
2018-06-13 09:12:25 +0530

    tab search working

--- angular-stuff/src/app/component/tab-search/tab-search.component.ts
+++ angular-stuff/src/app/component/tab-search/tab-search.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+
+import { FetchDataService } from '../../services/fetch-data.service';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-tab-search',
   templateUrl: './tab-search.component.html',
@@ -7,9 +11,88 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TabSearchComponent implements OnInit {
 
-  constructor() { }
+
+  inputPower: Number;
+  inputRunTime: Number;
+  inputUPSType: String;
+  inputRegion: String;
+  inputCountry: String;
+  result: Object;
+  powerFactorProperty = [
+    { value: 'Lag' },
+    { value: 'Lead' }
+  ];
+  bypassSlider: Boolean
+  powerFactorSlider: String;
+  powerFactorToggle: Boolean;
+
+  constructor(
+    private fetchDataService: FetchDataService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.result = null;
+    this.bypassSlider = true;
+    this.powerFactorSlider = "0.9"
+    this.powerFactorToggle = false;
   }
 
+
+
+searchUPS() {
+
+  console.log(this.inputRunTime)
+  console.log(this.inputPower)
+  var result;
+  var powerFactor;
+  var RUnit;
+
+
+  if(this.powerFactorToggle == false){
+    powerFactor = "Lag "+this.powerFactorSlider;
+  }
+
+  else{
+    powerFactor = "Lead "+this.powerFactorSlider;
+  }
+
+
+
+  //TODO
+  //need to fix bypass
+  const UPSparams = {
+    power: this.inputPower,
+    runtime: this.inputRunTime,
+    UPSType: this.inputUPSType,
+    Region: this.inputRegion,
+    Country: this.inputCountry,
+    Bypass: this.bypassSlider,
+    RUnit: RUnit,
+    PowerFactor: powerFactor,
+    // PowerFactorSign: this.inputPowerFactorSign,
+  }
+
+
+
+  console.log(UPSparams)
+
+
+
+
+  this.fetchDataService.getUPS(UPSparams).subscribe((data)=>{
+
+    //TODO
+   this.result=data;
+   console.log(data)
+   
+  },   
+  (error)=>{
+    console.log("Error: "+error);
+  })
+ 
+ 
 }
+}
+
+
