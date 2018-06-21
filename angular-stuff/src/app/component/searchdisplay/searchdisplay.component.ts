10aca746c2b023fcf6e8e64b0cd62bc63b1fc096
Diuda <divyanshu.1996@gmail.com>
2018-06-21 10:03:18 +0530

    changes in search display component

--- angular-stuff/src/app/component/searchdisplay/searchdisplay.component.ts
+++ angular-stuff/src/app/component/searchdisplay/searchdisplay.component.ts
@@ -1,11 +1,28 @@
-import { Component, OnInit,ViewChild } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild
+} from '@angular/core';
 
-import { FetchDataService } from '../../services/fetch-data.service';
-import { DisplayDataService } from '../../services/display-data.service';
-import { Router } from '@angular/router';
-import { getHostElement } from '@angular/core/src/render3';
-import {MatCardModule} from '@angular/material/card';
-import {MatPaginator, MatTableDataSource} from '@angular/material';
+import {
+  FetchDataService
+} from '../../services/fetch-data.service';
+import {
+  DisplayDataService
+} from '../../services/display-data.service';
+import {
+  Router
+} from '@angular/router';
+import {
+  getHostElement
+} from '@angular/core/src/render3';
+import {
+  MatCardModule
+} from '@angular/material/card';
+import {
+  MatPaginator,
+  MatTableDataSource
+} from '@angular/material';
 //import { SearchComponent } from '../search/search.component';
 @Component({
   selector: 'app-searchdisplay',
@@ -13,28 +30,42 @@ import {MatPaginator, MatTableDataSource} from '@angular/material';
   styleUrls: ['./searchdisplay.component.scss']
 })
 export class SearchdisplayComponent implements OnInit {
-  message:object;
-  result:string;
+  message: object;
+  result: string;
   displayedColumns = ['name', 'count'];
   compparts = [];
-  dataSource: MatTableDataSource<PeriodicElement>;
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  
+  dataSource: MatTableDataSource < PeriodicElement > ;
+  exist=true;
+  compnumber: number;
+  private paginator: MatPaginator;
+  @ViewChild(MatPaginator) set matPaginator(mp: MatPaginator) {
+    this.paginator = mp;
+    this.setDataSourceAttributes();
+  }
 
-  constructor(private displayDataService:DisplayDataService) { }
-  
-
-  ngOnInit() {
-    
-    
-    this.displayDataService.currentMessage.subscribe(message => this.message = message)
-     console.log(this.message['output'])
-    this.compparts =this.message['output'].parts;
-    this.dataSource = new MatTableDataSource<PeriodicElement>(this.compparts);
+  constructor(private displayDataService: DisplayDataService) {}
+  setDataSourceAttributes() {
     this.dataSource.paginator = this.paginator;
     
   }
-  
+
+  ngOnInit() {
+
+
+    this.displayDataService.currentMessage.subscribe(message => this.message = message)
+    // console.log(this.message['output'])
+    this.compparts = this.message['output'].parts;
+    console.log(this.compparts.length)
+    this.dataSource = new MatTableDataSource<PeriodicElement>(this.compparts);
+    // this.exist=true;
+    this.compnumber = this.compparts.length;
+    if (this.compnumber)
+      this.exist = true;
+     else
+      this.exist = false;
+
+  }
+
 
 
 }
@@ -42,5 +73,5 @@ export class SearchdisplayComponent implements OnInit {
 export interface PeriodicElement {
   name: string;
   count: number;
-  
+
 }
